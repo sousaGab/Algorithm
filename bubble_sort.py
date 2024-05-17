358221f232079c6cc27f59afd6ce5e9fbb5a81db
Gabriel Amaral Sousa <49481141+sousaGab@users.noreply.github.com>
2024-05-17 14:40:27 -0300

    up sorted array

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,1 +1,16 @@
+import random
+arr = []
+for x in range(10):
+    arr.append(random.randint(1, 100))
+sz = len(arr)-1
 
+print('unsorted array:')
+print(arr)
+
+for i in range(sz):
+    for j in range(sz-i):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print('sorted array:')
+print(arr)
